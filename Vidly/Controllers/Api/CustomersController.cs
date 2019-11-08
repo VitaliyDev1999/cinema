e8b983ad11456cddcdc18fab213b4ff96c5c58a7
Vitaliy <deinega.vit1@gmail.com>
2019-11-08 12:58:00 +0200

    add Web Api template

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Query;
+using System.Web.Http.OData.Routing;
+using Vidly.Models;
+using Microsoft.Data.OData;
+
+namespace Vidly.Controllers.api
+{
+    /*
+    Для класса WebApiConfig может понадобиться внесение дополнительных изменений, чтобы добавить маршрут в этот контроллер. Объедините эти инструкции в методе Register класса WebApiConfig соответствующим образом. Обратите внимание, что в URL-адресах OData учитывается регистр символов.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using Vidly.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Customer>("Customers");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class CustomersController : ODataController
+    {
+        private static ODataValidationSettings _validationSettings = new ODataValidationSettings();
+
+        // GET: odata/Customers
+        public IHttpActionResult GetCustomers(ODataQueryOptions<Customer> queryOptions)
+        {
+            // validate the query.
+            try
+            {
+                queryOptions.Validate(_validationSettings);
+            }
+            catch (ODataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            // return Ok<IEnumerable<Customer>>(customers);
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+
+        // GET: odata/Customers(5)
+        public IHttpActionResult GetCustomer([FromODataUri] int key, ODataQueryOptions<Customer> queryOptions)
+        {
+            // validate the query.
+            try
+            {
+                queryOptions.Validate(_validationSettings);
+            }
+            catch (ODataException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            // return Ok<Customer>(customer);
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+
+        // PUT: odata/Customers(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Customer> delta)
+        {
+            Validate(delta.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // TODO: Get the entity here.
+
+            // delta.Put(customer);
+
+            // TODO: Save the patched entity.
+
+            // return Updated(customer);
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+
+        // POST: odata/Customers
+        public IHttpActionResult Post(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // TODO: Add create logic here.
+
+            // return Created(customer);
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+
+        // PATCH: odata/Customers(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Customer> delta)
+        {
+            Validate(delta.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // TODO: Get the entity here.
+
+            // delta.Patch(customer);
+
+            // TODO: Save the patched entity.
+
+            // return Updated(customer);
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+
+        // DELETE: odata/Customers(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            // TODO: Add delete logic here.
+
+            // return StatusCode(HttpStatusCode.NoContent);
+            return StatusCode(HttpStatusCode.NotImplemented);
+        }
+    }
+}
